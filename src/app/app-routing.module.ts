8e0c6e7e8c7b94c8c360bf962a000d1ed0e9824e
Rosmery Chavez <you@example.comclear>
2020-07-18 09:42:44 -0500

    ...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { OrderComponent } from './components/waiter/order/order.component';
 
 
-
+const routes: Routes = [
+  { path: 'order', component: OrderComponent }
+];
 @NgModule({
   declarations: [],
   imports: [
+    [RouterModule.forRoot(routes)],
     CommonModule
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
+import { OrderComponent } from './components/waiter/order/order.component';
+import { PendingsComponent } from './components/waiter/pendings/pendings.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    OrderComponent,
+    PendingsComponent,
   ],
   imports: [
     BrowserModule,
