aee4e86be35ded3a3af732d763f755d2aacd89fc
luzma-coder <luz.marina.acdr@gmail.com>
2020-07-20 09:38:31 -0500

    new component order-pendings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { OrderComponent } from './components/waiter/order/order.component';
+import { PendingsComponent } from './components/waiter/pendings/pendings.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { OrderComponent } from './components/waiter/order/order.component';
     AppComponent,
     LoginComponent,
     OrderComponent,
+    PendingsComponent,
   ],
   imports: [
     BrowserModule,
