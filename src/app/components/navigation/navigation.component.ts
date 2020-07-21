4216f1b7cd72b19b183120ad5cd80e26cd85ef8f
Rosmery Chavez <you@example.comclear>
2020-07-21 00:52:37 -0500

    crea rutas hijas, vista mesero

---
+++ src/app/components/navigation/navigation.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+  active = 'top';
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
+  navigateToOrders(): void {
+    this.router.navigate(['/navigation/order']);
+  }
+  navigateToPendings(): void {
+    this.router.navigate(['/navigation/pendings']);
+  }
+  navigateToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+}
+
