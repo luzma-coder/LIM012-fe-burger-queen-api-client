8c556af06b8b7de580aff72b1950e108bd0f9d1c
Rosmery Chavez <you@example.comclear>
2020-08-13 09:09:28 -0500

    realiza lo restante para pintar los productos en pantala

--- src/app/components/admin/products/products/products.component.ts
+++ src/app/components/admin/products/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  arrProduct: Product[];
+
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
+    this.listProducts();
   }
 
+  listProducts(): void {
+    this.productsService.getProducts()
+      .subscribe(products => this.arrProduct = products);
+  }
+
+  addProduct(name: string, price: number, image: URL, type: string): void {
+    name = name.trim();
+    price = price.valueOf();
+    image = image;
+    type = type;
+    const objProduct = {
+      name,
+      price,
+      image,
+      type
+    };
+    if (!name) { return; }
+    this.productsService.postProduct(objProduct as Product)
+      .subscribe(dataproduct => {
+        console.log(dataproduct);
+        this.arrProduct.push(dataproduct);
+      });
+  }
+
+  updateProduct(product: Product): void {
+
+  }
+
+  deleteProduct(product: Product): void {
+    this.arrProduct = this.arrProduct.filter(data => data !== product);
+    this.productsService.deleteProduct(product).subscribe();
+  }
+
+  cancel(): void {
+
+  }
 }
